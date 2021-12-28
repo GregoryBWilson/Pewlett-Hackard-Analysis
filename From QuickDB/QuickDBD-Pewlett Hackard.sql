41b5dabeb3299380724245ff2c71e0a09f3b004a
Greg Wilson <greg@gbwilson.com>
2021-12-28 17:27:00 -0500

    Data imported BTW QuickDBD is literal and Postgres hates capitals for entities and attributes

--- From QuickDB/QuickDBD-Pewlett Hackard.sql
+++ From QuickDB/QuickDBD-Pewlett Hackard.sql
@@ -3,84 +3,81 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "Departments" (
-    "dept_no" VARHCAR(4)   NOT NULL,
-    "dept_name" VARHCAR(40)   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "dept_name" VARCHAR(40)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      ),
-    CONSTRAINT "uc_Departments_dept_name" UNIQUE (
+    CONSTRAINT "uc_departments_dept_name" UNIQUE (
         "dept_name"
     )
 );
 
-CREATE TABLE "Titles" (
+CREATE TABLE "titles" (
     "emp_no" INT   NOT NULL,
-    "title" VARHCAR   NOT NULL,
+    "title" VARCHAR   NOT NULL,
     "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
-        "emp_no"
-     )
+    "to_date" DATE   NOT NULL
 );
 
-CREATE TABLE "Salaries" (
+CREATE TABLE "salaries" (
     "emp_no" INT   NOT NULL,
     "salary" INT   NOT NULL,
     "from_date" DATE   NOT NULL,
     "to_date" DATE   NOT NULL,
-    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
         "emp_no"
      )
 );
 
-CREATE TABLE "Employees" (
+CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
     "birth_date" DATE   NOT NULL,
-    "first_name" VARHCAR   NOT NULL,
-    "last_name" VARHCAR   NOT NULL,
-    "gender" VARHCAR   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "gender" VARCHAR   NOT NULL,
     "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
+    CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
 );
 
-CREATE TABLE "Dept_Emp" (
+CREATE TABLE "dept_emp" (
     "emp_no" INT   NOT NULL,
-    "dept_no" VARHCAR(4)   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
     "from_date" DATE   NOT NULL,
     "to_date" DATE   NOT NULL,
-    CONSTRAINT "pk_Dept_Emp" PRIMARY KEY (
-        "emp_no"
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
      )
 );
 
-CREATE TABLE "Managers" (
-    "dept_no" VARHCAR(4)   NOT NULL,
+CREATE TABLE "managers" (
+    "dept_no" VARCHAR(4)   NOT NULL,
     "emp_no" INT   NOT NULL,
     "from_date" DATE   NOT NULL,
     "to_date" DATE   NOT NULL,
-    CONSTRAINT "pk_Managers" PRIMARY KEY (
+    CONSTRAINT "pk_managers" PRIMARY KEY (
         "dept_no","emp_no"
      )
 );
 
-ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "Managers" ADD CONSTRAINT "fk_Managers_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+ALTER TABLE "managers" ADD CONSTRAINT "fk_managers_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "Managers" ADD CONSTRAINT "fk_Managers_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "managers" ADD CONSTRAINT "fk_managers_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
